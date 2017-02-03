2b60a580ba8ff006f43a0d3d14f5253141716771
Dylan Vaughn <dylancvaughn@gmail.com>
2017-02-03 10:52:23 -0800

    adding lists of subnets to outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "public_subnets" {
+  value = "${aws_cloudformation_stack.vpc.outputs["PublicSubnet1"]},${aws_cloudformation_stack.vpc.outputs["PublicSubnet2"]},${aws_cloudformation_stack.vpc.outputs["PublicSubnet3"]}"
+}
+
 output "public_subnet_1" {
   value = "${aws_cloudformation_stack.vpc.outputs["PublicSubnet1"]}"
 }
@@ -8,6 +12,10 @@ output "public_subnet_2" {
 
 output "public_subnet_3" {
   value = "${aws_cloudformation_stack.vpc.outputs["PublicSubnet3"]}"
+}
+
+output "private_subnets" {
+  value = "${aws_cloudformation_stack.vpc.outputs["PrivateSubnet1"]},${aws_cloudformation_stack.vpc.outputs["PrivateSubnet2"]},${aws_cloudformation_stack.vpc.outputs["PrivateSubnet3"]}"
 }
 
 output "private_subnet_1" {
@@ -24,6 +32,10 @@ output "private_subnet_3" {
 
 output "spare_subnet_1" {
   value = "${aws_cloudformation_stack.vpc.outputs["SpareSubnet1"]}"
+}
+
+output "spare_subnets" {
+  value = "${aws_cloudformation_stack.vpc.outputs["SpareSubnet1"]},${aws_cloudformation_stack.vpc.outputs["SpareSubnet2"]},${aws_cloudformation_stack.vpc.outputs["SpareSubnet3"]}"
 }
 
 output "spare_subnet_2" {
